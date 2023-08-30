68935f6a7e5b810992053846c4011bd06694f94e
Antonio D'Angelo <reapercsp@gmail.com>
2023-08-30 09:08:38 +0200

    fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
-import './bootstrap';
+import "./bootstrap";
+
+import "~resources/scss/app.scss";
+
+import.meta.glob(["../img/**"]);
 
 import * as bootstrap from "bootstrap";
-
-import "/resources/scss/app.scss";
